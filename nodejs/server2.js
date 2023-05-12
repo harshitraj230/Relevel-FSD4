e37202b4165c5bcbe87bcbe40eb366db6a98abc7
Harshit Raj <harshitraj230@gmail.com>
2023-05-12 23:22:24 +0530

    dkynamic routes

---
+++ nodejs/server2.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const app=express();
+
+const db={
+    123:"Utkarsh",
+    567:"Rahul",
+    873:"Shrey"
+}
+
+app.get("/",function(req,res){
+    res.send("Express js backend http endpoint is ready !!!");
+});
+
+app.get("/name",function(req,res){
+    res.send("Express js backend http endpoint is ready! Name: Harshit Raj");
+})
+
+app.get("/users/:userId",function(req,res){
+    const userId=req.params.userId;
+    const UserDetails=db[userId]; 
+    res.send(`Hello from ${UserDetails}`);
+})
+
+app.listen(8000,()=>{
+    console.log("Application is listening on port 8000");
+})
