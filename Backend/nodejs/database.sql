568f91840b34792986e49296d7704e9d41c43a27
Harshit Raj <harshitraj230@gmail.com>
2023-05-16 02:41:44 +0530

    mySqlDatabase

---
+++ Backend/nodejs/database.sql
@@ -0,0 +1,32 @@
+-- insert into employee values (1234,'utkarsh','malik','utmalik@amazon.com','D37 Sector49 Gurgaon','Gurugram',300);
+
+-- select * from employee;
+
+-- insert into employee (employeeId,firstname,email,city,money) VALUES (345,'Rahul','rahul@gmail.com','Delhi',10000);
+
+
+-- insert into employees (employeeId,firstname,email,city,money) VALUES (345246,'Sjrey','shrey@gmail.com','Delhi',150000);
+
+
+
+
+-- alter table employee rename to employees;
+
+-- select * from employees;
+
+-- Update employees SET money=100000 where employeeid=1234;
+
+
+-- Update employees SET money=100000,city='Banglore' where employeeid=1234;
+
+
+-- Update employees SET money=100000,city='Hyderabad' where money<100000;
+
+-- select * from employees where city='Delhi' and money>100000;
+
+-- DELETE FROM employees where employeeid='345246';
+
+
+-- DELETE FROM employees where city='Hyderabad';
+
+-- select * from employees;
